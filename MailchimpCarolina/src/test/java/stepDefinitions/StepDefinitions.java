03ddd43e749355ee88d77dbf19415ce982c4e97c
ccarolina <carolinac@hotmail.se>
2021-04-07 16:39:32 +0200

    finjustering

--- MailchimpCarolina/src/test/java/stepDefinitions/StepDefinitions.java
+++ MailchimpCarolina/src/test/java/stepDefinitions/StepDefinitions.java
@@ -31,11 +31,13 @@ public class StepDefinitions {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://login.mailchimp.com/signup/");
+		
+	
 	}
 
 	@Given("I have entered  email {string} in the emailfield")
 	public void i_have_entered_email_in_the_emailfield(String Email) {
-		WebElement newemail = Wait(By.id("email"));
+		WebElement newemail = Wait(By.cssSelector ("input[id=email]"));
 
 		if (Email.equals("validemail")) {
 
@@ -53,7 +55,7 @@ public class StepDefinitions {
 	@Given("I have entered  username {string} in the usernamefield")
 	public void i_have_entered_username_in_the_usernamefield(String Username) {
 
-		WebElement newusername = Wait(By.id("new_username"));
+		WebElement newusername = Wait(By.cssSelector("input[id=new_username]"));
 
 		if (Username.equals("validusername")) {
 			Random randomuser = new Random();
@@ -78,7 +80,7 @@ public class StepDefinitions {
 
 	@When("I press Signup")
 	public void i_press_signup_buttom() {
-		WebElement signupbutton = Wait(By.id("create-account"));
+		WebElement signupbutton = Wait (By.id("create-account"));
 		signupbutton.click();
 
 	}
@@ -109,7 +111,7 @@ public class StepDefinitions {
 		// användarekonto skapas
 
 		  if (status.equals ("validinput")) {
-			WebElement Loginpage = Wait(By.className("margin-bottom"));
+			WebElement Loginpage = Wait(By.className("!margin-bottom--lv3 no-transform center-on-medium"));
 			assertEquals("Check your email", Loginpage.getText());
 			String URL = driver.getCurrentUrl();
 			equals (URL.startsWith("https://login.mailchimp.com/signup/success"));
